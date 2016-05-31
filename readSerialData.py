842086dc708ddef0d9ada4ee4d6c31a6a2d1e825
Sameer Dharsee <samyd79@yahoo.com>
2016-05-30 21:16:57 -0400

    Modified python program to have objects representing the microcontroller, allowing multiple devices if needed.

---
+++ readSerialData.py
@@ -0,0 +1,91 @@
+"""
+ReadLightAndTemp
+
+This file reads data sent to serial port and writes the data to a text file
+continuously.
+
+"""
+
+import os.path  # Import path method to direct to data file
+
+import sys  # Import sys module
+
+import time  # Import sleep method for delay
+
+import serial  # Import module to read serial port
+
+import cv2 # Import cv2 for image capture
+
+import sqlite3 # Import sqlite3 for storing data to database
+
+import serial_device # Import serial_device class
+
+# Declare string variable for controller device file
+device = "/dev/ttyACM0"
+
+# Declare device name
+name = "photonTest"
+
+# Declare device location
+location = "HomeDesktop"
+
+# Declare data fields as a dictionary [Field name: Field type]
+dataFields = {'DateAndTime' : 'TEXT', 'Light_Value' : 'REAL', 'Temp_C' : 'FLOAT'}
+
+# Declare string variable for path to save file
+###savePath = "/home/sdharsee/Projects/ReadLightAndTempProject/Data/"
+
+# Declare object for controller port, with baud rate 9600, and timeout of 9600
+databasePath = 'lightAndTempDatabase.db'
+
+# Declare object for controller device
+controller = serial_device.DataDevice(device, name, location, dataFields, databasePath)
+
+###imageSavePath = savePath + "/Images"    # Declare path to images folder
+
+# If the directory where data and images should be saved do not exist, create them
+###if not os.path.exists(savePath):
+###    os.makedirs(savePath)   # Create data directory
+###    os.makedirs(imageSavePath)  # Create image directory
+###elif not os.path.exists(imageSavePath):
+###    os.makedirs(imageSavePath) # Create image directory
+
+# Prompt for name of data file
+###saveFile = "LightAndTemp_" + time.strftime("%Y%m%d-%H%M%S")
+
+# Declare path to save file
+###imageFilePath = os.path.join(savePath, imageSavePath)
+
+###cam = cv2.VideoCapture(1)  # Initialize web cam. 1 is the index for the web cam
+
+# Loop to input data
+while True:
+    try:
+        # Update currentDataPoint
+        
+        ###currentDataPoint[0], currentDataPoint[1] = currentDataPoint[0].decode(encoding='utf-8'), currentDataPoint[1].decode(encoding='utf-8')
+        
+        #Add DateAndTime to the beginning of the list
+                
+        # Set image name to be currentDateTime
+        ###imageName = currentDateTime + ".jpg"
+        
+        # Read image from camera
+        ###retval, image = cam.read()
+        
+        # Save the image
+        ###cv2.imwrite(imageSavePath + "/" + imageName, image)
+        
+        # Delay for 0.5 second(s)
+        ###time.sleep(0.5)
+        
+        # Store data into lightAndTempDatabase
+        controller.dataEntry()
+        
+    except KeyboardInterrupt:
+        break
+
+###del(cam)
+controller.dbCursor.close()
+controller.dbConn.close()
+sys.exit()
---
+++ serial_device.py
@@ -0,0 +1,105 @@
+#!python
+
+""" 
+This class file will be used to create objects
+for each serial port being read from.
+
+"""
+
+import time
+import serial
+import sqlite3
+
+class DataDevice(object):
+    """A class that makes devices from serial ports
+       
+       device: String of path to device file
+       location: String of physical device location
+       dataFields: Dictionary of sensors on device as key
+                   and data SQL data type as value
+       databasePath: String of path to database
+       
+    """
+    def __init__(self, device, name, location, dataFields, databasePath):
+        self.device = device
+        self.deviceName = name
+        self.location = location
+        self.dataFields = dataFields
+        self.dataTable = name + "_" + location + "Table"
+        self.dbPath = databasePath
+        
+        self.port = self.serialPort()
+        self.createDataTable()
+        
+    
+    def description(self):
+        """Print out the DataDevice features
+        """
+        print(self.device)
+        print(self.deviceName)
+        print(self.location)
+        print(self.dataFields)
+    
+    def serialPort(self):
+        """Define which port the device is connected to,
+           the baud rate, and time between signals.
+        """
+        ser = serial.Serial(self.device, 9600, timeout=500)
+        return ser
+    
+    def createDataTable(self):
+        """Create a data table in the database if it does not exist yet.
+        """
+        self.dataTableHeaders = self.createTableHeaders()
+        self.dbConn = sqlite3.connect(self.dbPath)
+        self.dbCursor = self.dbConn.cursor()
+        self.dbCursor.execute('CREATE TABLE IF NOT EXISTS ' + self.dataTable + "(" + self.dataTableHeaders + ");")
+        
+        print("Data table is created.")
+    
+    def createTableHeaders(self):
+        """Create data table headers for table in database
+        """
+        counter = 0
+        self.dataTableHeaders = ''
+        for key, value in self.dataFields.items():
+            if counter < (len(self.dataFields)-1):
+                self.dataTableHeaders += key + ' ' + value + ', '
+                counter += 1
+            else:
+                self.dataTableHeaders += key + ' ' + value
+        
+        return self.dataTableHeaders
+    
+    def dataEntry(self):
+        """This function is to check length of dataList against number of columns in table.
+           After confirming correct length of dataList, enter new row into dataTable or
+           ask for the correct number of attributes in the entry.
+        """
+        
+        dataList = self.dataRead()
+        executionString = "INSERT INTO " + self.dataTable + " VALUES("
+        if len(dataList) == len(self.dataFields):
+            for i, item in enumerate(dataList[:-1]):
+                item = dataList[i]
+                executionString += dataList[i] + ', '
+            executionString += dataList[-1] + ")"
+            self.dbCursor.execute(executionString)
+            
+        else:
+            print("The number of data attributes being entered does not match the number of columns in the table.")
+    
+    def dataRead(self):
+        """Read the data from serial port.
+        """
+        currentDataPoint = self.port.readline().split()
+        for i, item in enumerate(currentDataPoint):
+            item = currentDataPoint[i].decode(encoding='utf-8')
+            currentDataPoint[i] = item
+
+        currentDataPoint.reverse()
+        currentDataPoint.append(time.strftime("%Y%m%d-%H%M%S"))
+        currentDataPoint.reverse()
+        
+        print(currentDataPoint)
+        return currentDataPoint
